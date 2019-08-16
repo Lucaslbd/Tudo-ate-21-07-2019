da7d9f6899cc507146f268915a984502b008c9a0
Lucas Bertoldi Dias <43075685+Lucaslbd@users.noreply.github.com>
2019-08-16 16:36:38 -0300

    Update ConexaoBD.java

--- NetBeans/Java web/AppContatos/src/java/conexao/ConexaoBD.java
+++ NetBeans/Java web/AppContatos/src/java/conexao/ConexaoBD.java
@@ -14,15 +14,5 @@ public class ConexaoBD {
         DriverManager.registerDriver(new org.postgresql.Driver());
         Connection conexao = DriverManager.getConnection(URL, USUARIO, SENHA);
         return conexao;
-    }
-
-    // private static final String USUARIO = "jlsistemasdege";
-    //  private static final String SENHA = "14811409lbd";
-    //  private static final String URL = "jdbc:mysql://mysql.jlsistemasdegestao.com.br/jlsistemasdege";
-
-    /*public static Connection conectar() throws SQLException {
-        DriverManager.registerDriver(new com.mysql.jdbc.Driver());
-        Connection conexao = DriverManager.getConnection(URL, USUARIO, SENHA);        
-        return conexao;
-    }*/
+    }    
 }
