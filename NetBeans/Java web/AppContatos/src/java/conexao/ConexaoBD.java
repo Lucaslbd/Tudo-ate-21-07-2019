e4dae069d5f5293429d31b2a47909d6fa70eabd6
Lucas Bertoldi Dias <43075685+Lucaslbd@users.noreply.github.com>
2019-08-16 16:36:19 -0300

    Update ConexaoBD.java

--- NetBeans/Java web/AppContatos/src/java/conexao/ConexaoBD.java
+++ NetBeans/Java web/AppContatos/src/java/conexao/ConexaoBD.java
@@ -6,9 +6,9 @@ import java.sql.SQLException;
 
 public class ConexaoBD {
     
-    private static final String USUARIO = "jlsistemasdegestao4";
-    private static final String SENHA = "14811409lbd";
-    private static final String URL = "jdbc:postgresql://pgsql.jlsistemasdegestao.com.br/jlsistemasdegestao4";
+    private static final String USUARIO = "";
+    private static final String SENHA = "";
+    private static final String URL = "";
 
     public static Connection conectar() throws SQLException {
         DriverManager.registerDriver(new org.postgresql.Driver());
